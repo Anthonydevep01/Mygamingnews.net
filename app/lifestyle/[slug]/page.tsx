0a8daa16efa252a39389a9e1307423ecfadad42e
Anthonydevep01 <antonypac2000@gmail.com>
2025-12-12 00:44:21 -0600

    Update metadata for all category pages and verify game awards article

--- app/lifestyle/[slug]/page.tsx
+++ app/lifestyle/[slug]/page.tsx
@@ -27,6 +27,28 @@ export async function generateMetadata({ params }: ArticlePageProps) {
     title: article.meta_title ? `${article.meta_title} - MyGamingNews.net` : `${article.title} - MyGamingNews.net`,
     description: article.meta_description || article.description,
     keywords: article.primary_keyword ? [article.primary_keyword, ...(article.secondary_keywords || [])].join(', ') : undefined,
+    openGraph: {
+      title: article.meta_title || article.title,
+      description: article.meta_description || article.description,
+      url: `https://www.mygamingnews.net/lifestyle/${article.slug}`,
+      siteName: 'MyGamingNews.net',
+      locale: 'en_US',
+      type: 'article',
+      images: [
+        {
+          url: article.image.startsWith('http') ? article.image : `https://www.mygamingnews.net${article.image.startsWith('/') ? '' : '/'}${article.image}`,
+          width: 1200,
+          height: 630,
+          alt: article.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: article.meta_title || article.title,
+      description: article.meta_description || article.description,
+      images: [article.image.startsWith('http') ? article.image : `https://www.mygamingnews.net${article.image.startsWith('/') ? '' : '/'}${article.image}`],
+    },
   }
 }
 
